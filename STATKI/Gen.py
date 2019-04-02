5383a32fd3895a8efa1324e5a2a43730ffce894f
Jędrzej Wiśniewski <jedrekwisniewski@wp.pl>
2019-04-02 20:03:12 +0200

    Create Gen.py
    Gra w statki : dodaje funkcję losowania jednomasztowca

---
+++ STATKI/Gen.py
@@ -0,0 +1,39 @@
+import numpy as np
+import random as ran
+
+plansza = np.zeros(shape=(10,10))
+def one():
+  
+    if True:
+        #poziom = ran.randint(0,1)
+        #if poziom == 0:
+            #x = ran.randint(0, len(plansza)-1)
+           # y = ran.randint(0, len(plansza)-1)
+           # xy = []
+          ##  xy.append(x)
+         #   xy.append(y)
+        
+        #else:
+        x = ran.randint(0, len(plansza)-1)
+        y = ran.randint(0, len(plansza)-1)
+        
+    try:
+        if plansza[y,x] == 0:
+
+            if plansza[y+1,x] != 0:
+                return one()
+            elif plansza[y-1,x] != 0:
+                return one()
+            elif plansza[y,x+1] != 0:
+                return one()
+            elif plansza[y,x-1] != 0:
+                return one()
+            else:
+                plansza[y,x] = 1
+    except IndexError:
+        return one()
+    
+        
+    return print(plansza),print("Poziomo :",x,"\nPionowo :",y)
+
+
