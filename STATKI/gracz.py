d1b40376fda4de6ea5c00e1079a52f2d8dc68788
izakosman <47474166+izakosman@users.noreply.github.com>
2019-04-16 13:02:44 +0200

    Update gracz.py

--- STATKI/gracz.py
+++ STATKI/gracz.py
@@ -1,5 +1,3 @@
-import numpy as np
-from numpy import *
 
 
 #GRACZ______________________________________________________________________________________________________
@@ -11,7 +9,7 @@ from numpy import *
 #gracz2.close()
 
 gracz1=[0, 0, 0, 0, 0, 0, 0, 2, 2, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 1, 0, 0, 0, 0, 0, 0, 3, 0],[0, 0, 0, 0, 0, 0, 0, 0, 3, 0],[3, 3, 3, 0, 0, 0, 0, 0, 3, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 4, 4, 4, 4, 0, 0, 0, 0],[1, 0, 0, 0, 0, 0, 0, 1, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 1],[2, 2, 0, 0, 2, 2, 0, 0, 0, 0]
-#print(gracz1)
+
 
 print("Alfabet :") #petelka co to zrobi zbior alfabetu
 x = 0
@@ -24,6 +22,7 @@ for i in range(65, 91):
         x = 0
 print(tmp)
 
+#ustawianie planszy........................ #gdyby generator generowal inaczej to i tak dobrze ustawi i wiersze i kolumny
 ilewierszy=[]
 wiersze=[]
 ilekolumn=[]
@@ -41,13 +40,11 @@ print("liczba wierszy: ",ilewierszy)
 print("lista wierszy: ",wiersze)
 print("lista kolumn: ",kolumny)
 
-
-
 #ustawianie statakow........................
 
-#jak odroznia statki
-#jesli elemt nastepny i/lub poprzedni  z wiersza jest rowny  i rozny od zera
-#jesli element i jest rowny  i rozny od zera w kolenym i/lub poprzednim  wierszu i rozny od zera
+#jakodroznia statki
+#czyli  jesli elemt nastepny z listy jest rowny  i rozny od zeratzn ze to statek
+#jesli element i jest rowny  i rozny od zera w kolenym wierszu to jest to statek
 
 
 listawszst=[]
@@ -55,11 +52,32 @@ statek1m=[]
 statek2m=[]
 
 
-def jakimasztowiec(gracz1):#chcialam zeby funkcja lecciala po liscie gracz1
-    for i in range(0,len(kolumny)*len(wiersze)+1):#zeby sprawdzalo dla kazdej zmiennej  
-        lsitawszst=gracz1[i]#  zeby jako liste tymczasowo wrzucalo podliste gracz1 tzn caly wiersz 
+def jakimasztowiec():#chcialam zeby funkcja lecciala po liscie gracz1
+    for i in range(0,len(kolumny)*len(wiersze)+1):#zeby sprawdzalo dla kazdej zmiennej
+        lsitawszst=gracz1[i]#  zeby jako liste tymczasowo wrzucalo podliste gracz1 tzn caly wiersz
         if listawszst[i]==2 : #  jesli zmienna jest 2 to wrzuc ja do dwumasztowcow
            statek2m.append(wiersze[i])
+           return  statek2m
 
-#NIE WYWALA BLEDU ALE JAK DAJE RETURNA TO NIE WYRZUCA NIC XD
+#sprawdzam tylko czy pracuje na dobrych zmiennych
 
+'''
+do zatopienia wszystkich okretow przeciwnika wg to bedzie sumowalo wszystkie statki, jesli jestto dwumasztowiec niech odejmuje 2 od sumy az dojdzie do 0
+
+porownywac liste atakow z lista polozonych statkow
+jesli strzal trafiony to odejmujesz jego sume od 0
+mozna zrobic wszystkie elementy += gracz1[i]
+
+oddanie strzalu to chyba tak samo jak generator natomiast 1 po 1 i jakos inaczej oznaczone-> miejsce strzalu porownywac z lista statku i if != 0 to trafiony, jesli == 0 pudlo
+jesli trafiony to sprawdza ktory element jakiego statku i odejmuje od sumy 
+
+#odebranie strzalu tj to samo
+
+#pudlo==ruch przeciwnika
+#trafiony==rzucaj jeszcze raz
+
+to z tym ctrl c nie  rozumiem 
+
+
+
+'''
