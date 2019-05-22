fe48b285a047cc8b2a0d4cde47b94bd2f58bd70b
andrzejsamson <andrzej.samson95@gmail.com>
2019-05-22 15:03:37 +0200

    Changed colors in 'wyswietlacz.py'
    Now it shows the color right, we only need to change the values on the axees

--- STATKI/wyswietlacz.py
+++ STATKI/wyswietlacz.py
@@ -2,20 +2,23 @@ import numpy as np
 import matplotlib as m
 import matplotlib.pylab as plt
 
-cdict = {
-    'blue' : (0),
-    'red' : (-1,-2,-3,-4),
-    'gray' : (1,2,3,4)}
-
-colors = [(0,0,1), (0,1,0), (1,0,0)]
+colors = ['red','red','red','red','blue','gray','gray','gray','gray']
 cmap_name = 'my_colormap'
 cm = m.colors.LinearSegmentedColormap.from_list(cmap_name, colors)
 
 matrix = np.load('Gracz1.npy')
-fig = plt.figure()
+fig = plt.figure(facecolor='black')
 ax = fig.add_subplot(1,1,1)
 ax.set_aspect('equal')
-plt.imshow(matrix,cmap=cm)
+ax.spines['bottom'].set_color('white')
+ax.spines['top'].set_color('white')
+ax.spines['right'].set_color('white')
+ax.spines['left'].set_color('white')
+ax.xaxis.label.set_color('white')
+ax.yaxis.label.set_color('white')
+ax.tick_params(axis='x', colors='white')
+ax.tick_params(axis='y', colors='white')
+plt.imshow(matrix,cmap=cm,vmin=-4,vmax=4)
 plt.colorbar()
+plt.grid(True)
 plt.show()
-
