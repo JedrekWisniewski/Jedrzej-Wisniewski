1099c9329303818dce5c6d265b9dbc7e213c585d
andrzejsamson <andrzej.samson95@gmail.com>
2019-05-24 00:34:51 +0200

    Update wyswietlacz.py
    axes labels are now on the top, bottom, right and left

--- STATKI/wyswietlacz.py
+++ STATKI/wyswietlacz.py
@@ -23,12 +23,13 @@ def wyswietlacz(macierz):
     ax.yaxis.set_minor_locator(minorLocator)
     ax.xaxis.set_minor_locator(minorLocator)
     ax.grid(which='minor', color='black')
+    ax.tick_params(axis='x', bottom=True, top=True, labelbottom=True, labeltop=True)
+    ax.tick_params(axis='y', right=True, left=True, labelright=True, labelleft=True)
+    
     x = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     plt.imshow(matrix,cmap=cm,vmin=-5,vmax=4)
     labels = range(0,10)
     plt.yticks(labels,[1,2,3,4,5,6,7,8,9,10])
     plt.xticks(labels,[x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9]])
-    plt.colorbar()
-    plt.gca().xaxis.tick_top()
     plt.show()
 
