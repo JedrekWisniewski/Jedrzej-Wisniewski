f4c8cff504376ccf6adea45ce423ff2e0e20248b
Jędrzej Wiśniewski <jedrekwisniewski@wp.pl>
2019-06-08 12:06:57 +0200

    Update Gracz by J.py
    Updated some functions
    
    Co-Authored-By: andrzejsamson <andrzejsamson@users.noreply.github.com>

--- STATKI/Gracz by J.py
+++ STATKI/Gracz by J.py
@@ -4,7 +4,51 @@ import numpy as np
 import sys
 import random as r
 
+sys.setrecursionlimit(5000)
 
+ruchy = ["a01","a02","a03","a04","a05","a06","a07","a08","a09","a10",
+         "b01","b02","b03","b04","b05","b06","b07","b08","b09","b10",
+         "c01","c02","c03","c04","c05","c06","c07","c08","c09","c10",
+         "d01","d02","d03","d04","d05","d06","d07","d08","d09","d10",
+         "e01","e02","e03","e04","e05","e06","e07","e08","e09","e10",
+         "f01","f02","f03","f04","f05","f06","f07","f08","f09","f10",
+         "g01","g02","g03","g04","g05","g06","g07","g08","g09","g10",
+         "h01","h02","h03","h04","h05","h06","h07","h08","h09","h10",
+         "i01","i02","i03","i04","i05","i06","i07","i08","i09","i10",
+         "j01","j02","j03","j04","j05","j06","j07","j08","j09","j10"]
+
+ruchy2 = ["a01","a02","a03","a04","a05","a06","a07","a08","a09","a10",
+         "b01","b02","b03","b04","b05","b06","b07","b08","b09","b10",
+         "c01","c02","c03","c04","c05","c06","c07","c08","c09","c10",
+         "d01","d02","d03","d04","d05","d06","d07","d08","d09","d10",
+         "e01","e02","e03","e04","e05","e06","e07","e08","e09","e10",
+         "f01","f02","f03","f04","f05","f06","f07","f08","f09","f10",
+         "g01","g02","g03","g04","g05","g06","g07","g08","g09","g10",
+         "h01","h02","h03","h04","h05","h06","h07","h08","h09","h10",
+         "i01","i02","i03","i04","i05","i06","i07","i08","i09","i10",
+         "j01","j02","j03","j04","j05","j06","j07","j08","j09","j10"]
+
+slownik1 = {0:"a",
+           1:"b",
+           2:"c",
+           3:"d",
+           4:"e",
+           5:"f",
+           6:"g",
+           7:"h",
+           8:"i",
+           9:"j"}
+
+slownik2 = {"a":0,
+           "b":1,
+           "c":2,
+           "d":3,
+           "e":4,
+           "f":5,
+           "g":6,
+           "h":7,
+           "i":8,
+           "j":9}
 
 def wczytywanie(nazwapliku="Gracz1.npy"):
     mymap = np.load(nazwapliku)
@@ -20,23 +64,18 @@ class Czteromaszt():
         if enemyfire == self.a:
             print("Trafiony")
             self.a = -1
-            return "4t"
         elif enemyfire == self.b:
             print("Trafiony")
             self.b = -1
-            return "4t"
         elif enemyfire == self.c:
             print("Trafiony")
             self.c = -1
-            return "4t"
         elif enemyfire == self.d:
             print("Trafiony")
             self.d = -1
-            return "4t"
         if self.a == -1 and self.b == -1 and self.c == -1 and self.d == -1:
             self.a = "już zatopiony"
             print ("Czteromasztowiec zatopiony")
-            return "4z"
             
     
 class Trojmaszt():
@@ -48,19 +87,15 @@ class Trojmaszt():
         if enemyfire == self.a:
             print("Trafiony")
             self.a = -1
-            return "3t"
         elif enemyfire == self.b:
             print("Trafiony")
             self.b = -1
-            return "3t"
         elif enemyfire == self.c:
             print("Trafiony")
             self.c = -1
-            return "3t"
         if self.a == -1 and self.b == -1 and self.c == -1:
             self.a = "już zatopiony"
             print("Zatopiony trójmasztowiec")
-            return "3z"
 
 class Dwumaszt():
     def __init__(self,a,b):
@@ -70,15 +105,12 @@ class Dwumaszt():
         if enemyfire == self.a:
             print("Trafiony")
             self.a = -1
-            return "2t"
         elif enemyfire == self.b:
             print("Trafiony")
             self.b = -1
-            return "2t"
         if self.a == -1 and self.b == -1:
             self.a = "już zatopiony"
             print("Zatopiony dwumasztowiec")
-            return "2z"
 
 class Jednomaszt():
     def __init__(self,a):
@@ -87,33 +119,31 @@ class Jednomaszt():
         if enemyfire == self.a:
             print ("Trafiony")
             self.a = -1
-            return "1t"
         if self.a == -1:
             self.a = "już zatopiony"
             print ("Zatopiony jednomasztowiec")
-            return "1z"
 
 def tworzenie():
     #czteromasztowiec
-    y,x = np.where(mymap == 4)
+    x,y = np.where(mymap == 4)
     cztm = Czteromaszt((y[0],x[0]),(y[1],x[1]),(y[2],x[2]),(y[3],x[3]))
     #trójmasztowce
-    y,x = np.where(mymap == 3)
+    x,y = np.where(mymap == 3)
     trz1 = Trojmaszt((y[0],x[0]),(y[1],x[1]),(y[2],x[2]))
     trz2 = Trojmaszt((y[3],x[3]),(y[4],x[4]),(y[5],x[5]))
     #dwumasztowce
-    y,x = np.where(mymap == 2)
+    x,y = np.where(mymap == 2)
     dwu1 = Dwumaszt((y[0],x[0]),(y[1],x[1]))
     dwu2 = Dwumaszt((y[2],x[2]),(y[3],x[3]))
     dwu3 = Dwumaszt((y[4],x[4]),(y[5],x[5]))
     #jednomasztowce
-    y,x = np.where(mymap == 1)
+    x,y = np.where(mymap == 1)
     jed1 = Jednomaszt((y[0],x[0]))
     jed2 = Jednomaszt((y[1],x[1]))
     jed3 = Jednomaszt((y[2],x[2]))
     jed4 = Jednomaszt((y[3],x[3]))
     return cztm ,trz1, trz2, dwu1, dwu2, dwu3, jed1, jed2, jed3, jed4
-
+"""
 #Lista w której zapisywane są koordynaty strzałów            
 ourshots = []
 #Strzał losowy, koordynaty
@@ -123,21 +153,44 @@ def random_shoot():
     return strzalpion, strzalpoz
 
 #Sprawdza czy strzał dozwolony, jeśli tak, wypluwa koordynaty , a strzał zapisuje do listy
+
 def fire():    
     x = random_shoot()
     if x in ourshots:
         return fire()
     else:
         ourshots.append(x)
-        return x
+        if x[1] < 9:
+            print(str(slownik1[x[0]]) + "0" + str(x[1]+1))
+            return (str(slownik1[x[0]]) + "0" + str(x[1]+1))
+        else:
+            print(str(slownik1[x[0]]) + "10")
+            return (str(slownik1[x[0]]) + "10")
+"""
+def fire():
+    y = len(ruchy) - 1
+    x = r.randint(0, y)
+    strzal = ruchy[x]
+    print(strzal, "||||||||||", y)
+    ruchy.pop(x)
+    return(strzal)
 
+def fire2():
+    y = len(ruchy2) - 1
+    x = r.randint(0, y)
+    strzal = ruchy2[x]
+    print(strzal, "----------", y)
+    ruchy2.pop(x)
+    return(strzal)
+    
 # Iteruje po każdym statku, wywołując metodę "trafiony" względem "enemyfire"=koordynatów
 def obrywamy(enemyfire):
     for statek in flota:
         statek.trafiony(enemyfire)
-    if mymap[enemyfire] == 0:
+    a = enemyfire[0]
+    b = enemyfire[1]
+    if mymap[b,a] == 0:
         print ("Pudło")
-        return "p"
 
 #TESTY######
 def test(strzaltest = 10):
@@ -153,8 +206,43 @@ cztm, trz1, trz2, dwu1, dwu2, dwu3, jed1, jed2, jed3, jed4 = tworzenie()
 flota = [cztm, trz1, trz2, dwu1, dwu2, dwu3, jed1, jed2, jed3, jed4]
 
 czytrafione = []
-pierwszenstwo = "s"
-def wielka_gra():
+pierwszenstwo = sys.argv[1]
+
+def proba():
+    if pierwszenstwo == "-s":
+        fire()
+        enemyfire = fire2()
+        a = enemyfire[0]
+        a = slownik2[a]
+        b = int(enemyfire[1])
+        if b == 0:
+            c = int(enemyfire[2])-1
+        else:
+            c = 9
+        obrywamy((a,c))
+        if cztm.a == "już zatopiony" and trz1.a == "już zatopiony" and trz2.a == "już zatopiony" and dwu1.a == "już zatopiony" and dwu2.a == "już zatopiony" and dwu3.a == "już zatopiony" and jed1.a == "już zatopiony" and jed2.a == "już zatopiony" and jed3.a == "już zatopiony" and jed4.a == "już zatopiony":
+            print("koniec")
+            sys.exit()
+        return proba()
+    
+    else:
+        enemyfire = fire()
+        a = enemyfire[0]
+        a = slownik2[a]
+        b = int(enemyfire[1])
+        if b == 0:
+            c = int(enemyfire[2])-1
+        else:
+            c = 9
+        obrywamy((a,c))
+        fire2()
+        if cztm.a == "już zatopiony" and trz1.a == "już zatopiony" and trz2.a == "już zatopiony" and dwu1.a == "już zatopiony" and dwu2.a == "już zatopiony" and dwu3.a == "już zatopiony" and jed1.a == "już zatopiony" and jed2.a == "już zatopiony" and jed3.a == "już zatopiony" and jed4.a == "już zatopiony":
+            print("koniec")
+            sys.exit()
+        return proba()
+proba()
+
+"""def wielka_gra():
     while True :
         if pierwszenstwo == "s":
             fire()
@@ -170,6 +258,6 @@ def wielka_gra():
             fire()
             czytrafione.append(input())
             if cztm.a == "już zatopiony" and trz1.a == "już zatopiony" and trz2.a == "już zatopiony" and dwu1.a == "już zatopiony" and dwu2.a == "już zatopiony" and dwu3.a == "już zatopiony" and jed1.a == "już zatopiony" and jed2.a == "już zatopiony" and jed3.a == "już zatopiony" and jed4.a == "już zatopiony":
-                sys.exit()
+                sys.exit()"""
 
-wielka_gra()
+
